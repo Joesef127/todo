26e8723bbf98546e6d0930fd717c64bac44f3869
Adedayo Adegbola <adegboladayor@gmail.com>
2024-12-30 02:22:38 +0100

    feat: finished setting up intro page and started home page for creating tasks

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+    <nav className="flex items-center justify-between py-4 px-6 bg-white shadow-md">
+      <h1 className="text-2xl font-bold">To-Do List</h1>
+      {/* Add your navigation links here */}
+    </nav>
+  )
+}
--- src/pages/Intro.tsx
+++ src/pages/Intro.tsx
@@ -12,17 +12,17 @@ export default function Intro() {
       <div className="absolute inset-0 bg-black/80"></div>
 
       {/* Content */}
-      <div className="relative z-10 flex items-center justify-between flex-col h-full py-64">
+      <div className="relative z-10 flex items-center justify-between gap-8 flex-col h-full py-60">
         <div>
-          <h1 className="text-white text-[200px] leading-tight font-bold font-roboto">
+          <h1 className="text-white text-[200px] leading-tight font-bold font-roboto ">
             To Do
           </h1>
-          <p className="mt-4 text-4xl text-red-400 font-light text-center">
+          <p className="mt-3 text-4xl text-red-400 font-light text-center">
             Create And Manage <br /> Your Tasks Now.
           </p>
         </div>
         <button
-          className="px-6 py-3 border text-white border-white rounded-md hover:bg-white hover:text-black"
+          className="px-24 py-3 border text-white border-white rounded-full hover:bg-white hover:text-black transition ease-in-out duration-300"
           onClick={() => {
             navigate('/home');
           }}
