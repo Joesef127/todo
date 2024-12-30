8be7f841bc20135c3fbbd6ffdea13a3dabdf4633
Adedayo Adegbola <adegboladayor@gmail.com>
2024-12-31 00:25:53 +0100

    chore: edited site title

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,10 +6,10 @@ export default function Navbar() {
   return (
     <nav className="p-4 bg-white shadow-md flex items-center justify-center mx-auto w-full">
       <div className="max-w-lg w-full flex items-center justify-between">
-        <div className='flex gap-1'>
-          {/* <h1 className="text-2xl font-bold">To Do</h1> */}
+        <div className='flex gap-1 items-center justify-center'>
+          <h1 className="text-2xl font-bold">To Do</h1>
           <span>
-            <img src={task} alt="logo" className="w-10" />
+            <img src={task} alt="logo" className="w-6" />
           </span>
         </div>
         <button
